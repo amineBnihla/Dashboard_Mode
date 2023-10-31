e972d8f05cfb4871c958294375a72c8e14d771fa
amineBnihla <bnihla.10@gmail.com>
2023-10-31 23:10:23 +0100

    start nav menu

---
+++ components/Menu/Nav.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="min-w-[200px]">
+    <h2
+      class="text-14 text-myColor dark:text-white dark:text-opacity-60 text-opacity-60 mb-2 pl-3"
+    >
+      Dashboards
+    </h2>
+    <ul>
+      <li
+        class="hover:bg-myColor dark:hover:bg-white dark:hover:bg-opacity-10 hover:bg-opacity-5 rounded-lg mb-1"
+      >
+        <NuxtLink to="/" class="flex items-center gap-1 py-1">
+          <div class="min-w-[20px] ml-1 grid place-items-center">
+            <Icon
+              name="ic:round-keyboard-arrow-right"
+              size="20"
+              class="text-myColor dark:text-white dark:text-opacity-30 text-opacity-30"
+            />
+          </div>
+          <Icon
+            name="ph:chart-pie-slice"
+            size="20"
+            class="text-myColor mr-1 dark:text-white"
+          />
+          <span class="text-14 text-myColor dark:text-white">Default</span>
+        </NuxtLink>
+      </li>
+      <li
+        class="hover:bg-myColor dark:hover:bg-white dark:hover:bg-opacity-10 hover:bg-opacity-5 rounded-lg"
+      >
+        <NuxtLink to="/" class="text-myColor flex items-center gap-1 py-1">
+          <div class="min-w-[20px] ml-1 grid place-items-center">
+            <Icon
+              name="ic:round-keyboard-arrow-right"
+              size="20"
+              class="text-myColor dark:text-white dark:text-opacity-30 text-opacity-30"
+            />
+          </div>
+          <Icon
+            name="ph:shopping-bag-open-duotone"
+            size="20"
+            class="text-myColor dark:text-white mr-1"
+          />
+          <span class="text-14 dark:text-white">Ecommerce</span>
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+</style>
--- components/layout/SidebarDashboard.vue
+++ components/layout/SidebarDashboard.vue
@@ -1,15 +1,68 @@
 <template>
   <div
-    class="fixed w-0 h-screen transition-[width,left,right,background-color] duration-500 ltr:left-0 rtl:right-0 top-0 ltr:border-r rtl:border-l border-border bg-white dark:bg-myColor z-50 border-opacity-10 dark:border-white dark:border-opacity-10 overflow-hidden"
+    class="fixed w-0 px-4 h-screen transition-[width,left,right,background-color] duration-500 ltr:left-0 rtl:right-0 top-0 ltr:border-r rtl:border-l border-border bg-white dark:bg-myColor z-50 border-opacity-10 dark:border-white dark:border-opacity-10 overflow-hidden"
     :class="{ 'w-[212px]': modelValue }"
   >
-    <button
-      @click="$emit('update:modelValue', !modelValue)"
-      class="text-myColor dark:text-white visible lg:hidden"
-    >
-      X
-    </button>
-    SideBar
+    <div class="flex justify-end py-2 px-4">
+      <button
+        @click="$emit('update:modelValue', !modelValue)"
+        class="text-myColor dark:text-white visible lg:hidden"
+      >
+        X
+      </button>
+    </div>
+
+    <div class="flex gap-2 pl-2 items-center mb-7 min-w-[200px]">
+      <img
+        src="~/assets/images/ByeWind.png"
+        alt="image user"
+        srcset=""
+        class=""
+      />
+      <span class="text-14 text-myColor dark:text-white">ByeWind</span>
+    </div>
+    <div class="pl-3 min-w-[200px] mb-9">
+      <div class="flex gap-5">
+        <span
+          class="text-14 capitalize text-myColor dark:text-white dark:text-opacity-60 text-opacity-60"
+          >Favorite</span
+        >
+        <span
+          class="text-14 capitalize text-myColor dark:text-white dark:text-opacity-40 text-opacity-40"
+          >Recently</span
+        >
+      </div>
+      <ul class="py-2">
+        <li>
+          <NuxtLink
+            to="/"
+            class="text-14 text-myColor dark:text-white flex items-center"
+          >
+            <Icon
+              name="ci:dot-02-s"
+              size="26"
+              class="text-myColor dark:text-white dark:text-opacity-20 text-opacity-20 mr-1"
+            />
+            <span>Overview</span>
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink
+            to="/"
+            class="text-14 text-myColor dark:text-white flex items-center"
+          >
+            <Icon
+              name="ci:dot-02-s"
+              size="26"
+              class="text-myColor dark:text-white dark:text-opacity-20 text-opacity-20 mr-1"
+            />
+            <span>Projects</span>
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+    <MenuNav></MenuNav>
+    <div></div>
   </div>
 </template>
 
