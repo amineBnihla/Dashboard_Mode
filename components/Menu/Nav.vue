de66feaf6d0a1ea3811a162b7a69a9e48939299c
amineBnihla <bnihla.10@gmail.com>
2023-11-03 00:02:34 +0100

    nav open close

--- components/Menu/Nav.vue
+++ components/Menu/Nav.vue
@@ -3,53 +3,63 @@
     <h2
       class="text-14 text-myColor dark:text-white dark:text-opacity-60 text-opacity-60 mb-2 pl-3"
     >
-      Dashboards
+      {{ $t(links.title) }}
     </h2>
     <ul>
       <li
         class="hover:bg-myColor dark:hover:bg-white dark:hover:bg-opacity-10 hover:bg-opacity-5 rounded-lg mb-1"
+        v-for="(link, index) in links.items"
+        :key="index"
       >
-        <NuxtLink to="/" class="flex items-center gap-1 py-1">
-          <div class="min-w-[10px] overflow-hidden  grid place-items-center">
+        <NuxtLink
+          :to="link?.to"
+          class="flex items-center gap-[2px] py-1"
+          @click="openMenu = !openMenu"
+        >
+          <div class="min-w-[10px] overflow-hidden grid place-items-center">
             <Icon
+              v-if="link.submenu?.length > 0"
               name="ic:round-keyboard-arrow-right"
               size="20"
               class="text-myColor dark:text-white dark:text-opacity-30 text-opacity-30"
             />
           </div>
           <Icon
-            name="ph:chart-pie-slice"
+            :name="link?.icon"
             size="20"
-            class="text-myColor mr-1 min-w-[20px] dark:text-white"
+            class="text-myColor ltr:mr-1 rtl:ml-1 min-w-[20px] dark:text-white"
           />
-          <span class="text-14 text-myColor dark:text-white">Default</span>
+
+          <span class="text-14 text-myColor dark:text-white">{{
+            $t(link?.name)
+          }}</span>
         </NuxtLink>
-        <ul></ul>
+
+        <ul
+          class="mt-1 overflow-hidden"
+          :class="OpenSubMenu"
+          v-if="link.submenu?.length > 0"
+        >
+          <li class=" " v-for="(submenu, ind) in link.submenu" :key="ind">
+            <NuxtLink
+              :to="submenu.to"
+              class="py-1 block ltr:pl-12 rtl:pr-12 text-myColor dark:text-white text-14 hover:bg-myColor hover:bg-opacity-10 dark:hover:bg-white dark:hover:bg-opacity-5"
+            >
+              {{ $t(submenu.name) }}
+            </NuxtLink>
+          </li>
+        </ul>
       </li>
-      <!-- <li
-        class="hover:bg-myColor dark:hover:bg-white dark:hover:bg-opacity-10 hover:bg-opacity-5 rounded-lg"
-      >
-        <NuxtLink to="/" class="text-myColor flex items-center gap-1 py-1">
-          <div class="min-w-[20px] ml-1 grid place-items-center">
-            <Icon
-              name="ic:round-keyboard-arrow-right"
-              size="20"
-              class="text-myColor dark:text-white dark:text-opacity-30 text-opacity-30"
-            />
-          </div>
-          <Icon
-            name="ph:shopping-bag-open-duotone"
-            size="20"
-            class="text-myColor dark:text-white mr-1"
-          />
-          <span class="text-14 dark:text-white">Ecommerce</span>
-        </NuxtLink>
-      </li> -->
     </ul>
   </div>
 </template>
 
 <script setup>
+const props = defineProps(["links"]);
+const openMenu = ref(false);
+const OpenSubMenu = computed(() => {
+  return openMenu.value == true ? "min-h-0" : "h-0";
+});
 </script>
 
 <style>
--- components/layout/SidebarDashboard.vue
+++ components/layout/SidebarDashboard.vue
@@ -1,75 +1,102 @@
 <template>
   <div
-    class="fixed  h-screen transition-[width,left,right,background-color] duration-500 ltr:left-0 rtl:right-0 top-0 ltr:border-r rtl:border-l border-border bg-white dark:bg-myColor z-50 border-opacity-10 dark:border-white dark:border-opacity-10 overflow-hidden"
-    :class="[modelValue ? 'w-[212px] ' : 'w-0 '  ]"
+    class="fixed h-screen transition-[width,left,right,background-color] duration-500 ltr:left-0 rtl:right-0 top-0 ltr:border-r rtl:border-l border-border bg-white dark:bg-myColor z-50 border-opacity-10 dark:border-white dark:border-opacity-10 overflow-hidden"
+    :class="[modelValue ? 'w-[212px] ' : 'w-0 ']"
   >
-  <div class="mx-4">
-    <div class="flex justify-end py-2 ">
-      <button
-        @click="$emit('update:modelValue', !modelValue)"
-        class="text-myColor dark:text-white visible lg:hidden"
-      >
-        X
-      </button>
-    </div>
-
-    <div class="flex gap-2 pl-2 items-center mb-7 min-w-[200px]">
-      <img
-        src="~/assets/images/ByeWind.png"
-        alt="image user"
-        srcset=""
-        class=""
-      />
-      <span class="text-14 text-myColor dark:text-white">ByeWind</span>
-    </div>
-    <div class="pl-3 min-w-[200px] mb-9">
-      <div class="flex gap-5">
-        <span
-          class="text-14 capitalize text-myColor dark:text-white dark:text-opacity-60 text-opacity-60"
-          >Favorite</span
+    <div class="mx-4">
+      <div class="flex justify-end py-2">
+        <button
+          @click="$emit('update:modelValue', !modelValue)"
+          class="text-myColor dark:text-white visible lg:hidden"
         >
-        <span
-          class="text-14 capitalize text-myColor dark:text-white dark:text-opacity-40 text-opacity-40"
-          >Recently</span
-        >
+          X
+        </button>
       </div>
-      <ul class="py-2">
-        <li>
-          <NuxtLink
-            to="/"
-            class="text-14 text-myColor dark:text-white flex items-center"
+
+      <div class="flex gap-2 pl-2 items-center mb-7 min-w-[200px]">
+        <img
+          src="~/assets/images/ByeWind.png"
+          alt="image user"
+          srcset=""
+          class=""
+        />
+        <span class="text-14 text-myColor dark:text-white">ByeWind</span>
+      </div>
+      <div class="pl-3 min-w-[200px] mb-9">
+        <div class="flex gap-5">
+          <span
+            class="text-14 capitalize text-myColor dark:text-white dark:text-opacity-60 text-opacity-60"
+            >Favorite</span
           >
-            <Icon
-              name="ci:dot-02-s"
-              size="26"
-              class="text-myColor dark:text-white dark:text-opacity-20 text-opacity-20 mr-1"
-            />
-            <span>Overview</span>
-          </NuxtLink>
-        </li>
-        <li>
-          <NuxtLink
-            to="/"
-            class="text-14 text-myColor dark:text-white flex items-center"
+          <span
+            class="text-14 capitalize text-myColor dark:text-white dark:text-opacity-40 text-opacity-40"
+            >Recently</span
           >
-            <Icon
-              name="ci:dot-02-s"
-              size="26"
-              class="text-myColor dark:text-white dark:text-opacity-20 text-opacity-20 mr-1"
-            />
-            <span>Projects</span>
-          </NuxtLink>
-        </li>
-      </ul>
+        </div>
+        <ul class="py-2">
+          <li>
+            <NuxtLink
+              to="/"
+              class="text-14 text-myColor dark:text-white flex items-center"
+            >
+              <Icon
+                name="ci:dot-02-s"
+                size="26"
+                class="text-myColor dark:text-white dark:text-opacity-20 text-opacity-20 mr-1"
+              />
+              <span>Overview</span>
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink
+              to="/"
+              class="text-14 text-myColor dark:text-white flex items-center"
+            >
+              <Icon
+                name="ci:dot-02-s"
+                size="26"
+                class="text-myColor dark:text-white dark:text-opacity-20 text-opacity-20 mr-1"
+              />
+              <span>Projects</span>
+            </NuxtLink>
+          </li>
+        </ul>
+      </div>
+      <MenuNav :links="Links"></MenuNav>
     </div>
-    <MenuNav></MenuNav>
-   </div>
   </div>
 </template>
 
 <script setup>
 defineProps(["modelValue"]);
 defineEmits(["update:modelValue"]);
+const Links = {
+  title: "dashboard",
+  items: [
+    {
+      name: "default",
+      to: "/",
+      icon: "ph:chart-pie-slice",
+      submenu: [
+        {
+          to: "/",
+          name: "overview",
+        },
+      ],
+    },
+    {
+      name: "ecommerce",
+      to: "/",
+      icon: "ph:shopping-bag-open-duotone",
+      submenu: [
+        {
+          to: "/",
+          name: "overview",
+        },
+      ],
+    },
+  ],
+};
 </script>
 
 <style scoped>
